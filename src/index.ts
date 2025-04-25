b20669ae0b6b62119192fd401697ffba6b4c67fe
l11040 <l1104033837@gmail.com>
2025-04-25 22:40:33 +0900

    [Fix]

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "./index.css";
-
-export * from "./Button";
-export * from "./Tab";
-export * from "./TextArea";
-export * from "./Input";
-export * from "./Skeleton";
-export * from "./Modal";
-
 import "./types";
+
+export { default as Button } from "./Button";
+export { default as Tab } from "./Tab";
+export { default as TextArea } from "./TextArea";
+export { default as Input } from "./Input";
+export { default as Modal } from "./Modal";
+
+export * from "./Skeleton";
