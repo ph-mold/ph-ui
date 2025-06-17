cfc30e28ffea08e69cb3b64220ca4f8cb920a8be
l11040 <l1104033837@gmail.com>
2025-06-17 14:35:18 +0900

    [Update] alertAtom에서 alertQueueState 및 AlertState 타입을 내보내도록 수정

--- src/Alert/index.ts
+++ src/Alert/index.ts
@@ -1,3 +1,5 @@
 export { Alert } from "./Alert";
 export { GlobalAlert } from "./GlobalAlert";
 export { useAlert } from "./useAlert";
+export { alertQueueState } from "./alertAtom";
+export type { AlertState } from "./alertAtom";
