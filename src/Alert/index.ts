4fdaece93a1de587ff45bac2c810c0cf8a79c696
l11040 <l1104033837@gmail.com>
2025-06-17 14:37:45 +0900

    [Update] alertAtom에서 alertQueueState 및 AlertState 타입 내보내기 제거

--- src/Alert/index.ts
+++ src/Alert/index.ts
@@ -1,5 +1,3 @@
 export { Alert } from "./Alert";
 export { GlobalAlert } from "./GlobalAlert";
 export { useAlert } from "./useAlert";
-export { alertQueueState } from "./alertAtom";
-export type { AlertState } from "./alertAtom";
